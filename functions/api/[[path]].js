fede81debf45a66630c5fe92f8b8e6967f036e41
CNalks <19960214abc@163.com>
2026-03-25 09:13:40 +0800

    Point Pages proxy to TKE backend

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   var url = new URL(context.request.url);
-  var upstream = new URL(url.pathname + url.search, 'http://1.13.141.254');
+  var upstream = new URL(url.pathname + url.search, "http://129.211.228.122");
   var request = new Request(upstream.toString(), context.request);
   return fetch(request);
 }
